23c9131c0e3e350c55323bbb50b606d4956d6fd4
Heng Bodyka <heng.bodyka@sabay.com>
2020-10-25 12:58:30 +0700

    update menu to be responsive

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "gatsby";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/pro-light-svg-icons";
+import { faBars, faTimes } from "@fortawesome/pro-light-svg-icons";
 import menuStyles from "../styles/menu.module.css";
 
 const menuItems = [
@@ -27,27 +27,34 @@ export default function Menu() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header>
+    <header className="sm:flex sm:justify-between">
       <div className="flex items-center justify-end p-5">
-        <div>
+        <div className="sm:hidden">
           <button
             type="button"
             onClick={() => {
               setIsOpen(!isOpen);
             }}
           >
-            <FontAwesomeIcon icon={faBars} className="text-3xl" />
+            <FontAwesomeIcon
+              icon={faTimes}
+              className={(isOpen === true ? "block" : "hidden") + " text-3xl"}
+            />
+            <FontAwesomeIcon
+              icon={faBars}
+              className={(isOpen === false ? "block" : "hidden") + " text-3xl"}
+            />
           </button>
         </div>
       </div>
 
-      <div className={isOpen === true ? "block p-5" : "hidden"}>
+      <div className={(isOpen === true ? "block" : "hidden") + " p-10 sm:flex"}>
         {menuItems.map((item, index) => {
           return (
             <Link
               to={item.link}
               key={index}
-              className="block font-semibold my-5 text-2xl hover:bg-gray-800 rounded px-2"
+              className="block sm:inline-block font-semibold sm:font-thin sm:mx-10 my-5 sm:my-0 text-2xl hover:bg-gray-800 rounded px-2"
             >
               {item.page}
             </Link>
