a74d5f5a80c3b5404de4b5af9e450b41e313827c
Heng Bodyka <heng.bodyka@sabay.com>
2020-10-29 12:36:09 +0700

    wip: about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,26 @@
 import React from "react";
+import { Link } from "gatsby";
 import Layout from "../components/layout";
 import { Helmet } from "react-helmet";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBriefcase } from "@fortawesome/pro-light-svg-icons";
+import linkStyle from "../assets/styles/link.module.css";
+
+const workExperience = [
+  {
+    company: "MUCS by mo",
+    title: "Co-Founder",
+    timeline: "August 2018 - Present",
+    location: "Phnom Penh, Cambodia",
+    link: "https://www.instagram.com/mucsbymo/",
+  },
+  {
+    company: "Sabay Digital Corp.",
+    title: "Frontend Team Lead x UI/UX Lead",
+    timeline: "May 2018 - Present",
+    location: "Phnom Penh, Cambodia",
+  },
+];
 
 export default function About() {
   return (
@@ -28,8 +48,39 @@ export default function About() {
         <div className="sm:w-1/4"></div>
       </div>
       <hr className="my-8" />
-      <div>
-        <h1 className="text-4xl font-black">Qualifications</h1>
+      <h1 className="text-4xl font-black mb-5">Qualifications</h1>
+      <div className="flex">
+        <div className="w-1/2">
+          <h3 className="text-2xl">
+            <FontAwesomeIcon icon={faBriefcase} className="text-1xl mr-3" />
+            Experience
+          </h3>
+          {workExperience.map((item, index) => {
+            return (
+              <div key={index} className="my-8">
+                <div>
+                  <span className="font-bold text-red">{item.title}</span>
+                  <span className="mx-5">|</span>
+                  <span>{item.timeline}</span>
+                </div>
+                <div>
+                  {item.link ? (
+                    <Link
+                      to={item.link}
+                      target="_blank"
+                      className={linkStyle.link}
+                    >
+                      {item.company}
+                    </Link>
+                  ) : (
+                    item.company
+                  )}
+                </div>
+                <div className="text-xs">{item.location}</div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </Layout>
   );
