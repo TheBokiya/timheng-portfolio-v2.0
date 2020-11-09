57ff5735c19d4e1fbaad92fb6a038ca85ee82b68
Heng Bodyka <heng.bodyka@sabay.com>
2020-11-09 21:59:13 +0700

    movie-ratings page updated

--- src/pages/portfolio/movie-ratings.js
+++ src/pages/portfolio/movie-ratings.js
@@ -58,6 +58,52 @@ const outcome = [
 const liveSite = "https://github.com/TheBokiya/MovieProject";
 
 export default function MovieRatings({ data }) {
+  const overallImg = data.allFile.edges.find(
+    image => image.node.relativePath === "movie_ratings/movie_ratings-04.png"
+  );
+  const actionImg = data.allFile.edges.find(
+    image => image.node.relativePath === "movie_ratings/movie_ratings-05.png"
+  );
+  const animationImg = data.allFile.edges.find(
+    image => image.node.relativePath === "movie_ratings/movie_ratings-06.png"
+  );
+  const comedyImg = data.allFile.edges.find(
+    image => image.node.relativePath === "movie_ratings/movie_ratings-07.png"
+  );
+  const dramaImg = data.allFile.edges.find(
+    image => image.node.relativePath === "movie_ratings/movie_ratings-08.png"
+  );
+  const docuImg = data.allFile.edges.find(
+    image => image.node.relativePath === "movie_ratings/movie_ratings-09.png"
+  );
+  const romanceImg = data.allFile.edges.find(
+    image => image.node.relativePath === "movie_ratings/movie_ratings-10.png"
+  );
+  const shortImg = data.allFile.edges.find(
+    image => image.node.relativePath === "movie_ratings/movie_ratings-11.png"
+  );
+  const toggleImg = [
+    actionImg,
+    animationImg,
+    comedyImg,
+    dramaImg,
+    docuImg,
+    romanceImg,
+    shortImg,
+  ];
+
+  const hoverGif1 = data.allFile.edges.find(
+    gif => gif.node.relativePath === "movie_ratings/movie_ratings-01.gif"
+  );
+  const hoverGif2 = data.allFile.edges.find(
+    gif => gif.node.relativePath === "movie_ratings/movie_ratings-02.gif"
+  );
+  const moveGif = data.allFile.edges.find(
+    gif => gif.node.relativePath === "movie_ratings/movie_ratings-03.gif"
+  );
+  const titleGif = data.allFile.edges.find(
+    gif => gif.node.relativePath === "movie_ratings/movie_ratings-12.gif"
+  );
   return (
     <Layout>
       <Helmet>
@@ -104,25 +150,76 @@ export default function MovieRatings({ data }) {
             </div>
           </div>
           <hr className="my-5" />
-          {data.allFile.edges.map((item, index) => {
-            if (item.node.extension == "gif") {
-              return (
-                <img
-                  src={item.node.publicURL}
-                  key={index}
-                  className="my-5 w-full"
-                />
-              );
-            } else {
-              return (
-                <Img
-                  fluid={item.node.childImageSharp.fluid}
-                  key={index}
-                  className="my-5"
-                />
-              );
-            }
+          <p>
+            Before I started working on this project, I knew I wanted to work
+            with CSV data and JavaFX. I just wasn't sure what kind of data I'm
+            interested in working on. I came across a movie dataset in CSV file
+            format that has ratings information about movies and I felt that
+            this would be a fun and interesting project to work on. In this
+            post, I will discuss about the features implemented into the
+            visualization, approaches and challenges that came up during the
+            development process. Source code can be accessed through GitHub.
+          </p>
+          <Img
+            fluid={overallImg.node.childImageSharp.fluid}
+            alt="overall-ui"
+            className="mb-5"
+          />
+          <p>
+            The UI of the applcation is split into two parts, the canvas and the
+            control toolbar. The canvas holds all the drawings and manipulations
+            of the circles while the toolbar holds necessary UI control
+            components that allows the user to interact with the data. These are
+            the UI control components.
+          </p>
+          <ul>
+            <li>
+              <span className="font-bold">Year Slider</span>: allows the user to
+              control the year that movies are released between 1970-2005
+            </li>
+            <li>
+              <span className="font-bold">Show All</span>: would tell the
+              application to disable the year slider control and show all movies
+              between 1970-2005
+            </li>
+            <li>
+              <span className="font-bold">Genre Toggle Buttons</span>: toggle
+              the canvas between different genres, action, animation, comedy,
+              drama, comentary, romance and short film.
+            </li>
+          </ul>
+          {toggleImg.map((item, index) => {
+            return (
+              <Img
+                fluid={item.node.childImageSharp.fluid}
+                key={index}
+                className="my-5 w-full"
+              />
+            );
           })}
+          <p>
+            Various interactions are also implemented in this application.
+            Circles change the fill and stroke color when mouse is hovered over
+            and change back when the mouse leave.
+          </p>
+          <img src={hoverGif1.node.publicURL} className="my-5 w-full" />
+          <img src={hoverGif2.node.publicURL} className="my-5 w-full" />
+          <p>Circles can also be moved independently around the canvas.</p>
+          <img src={moveGif.node.publicURL} className="my-5 w-full" />
+          <p>
+            Last but not least, the title and rating of the movie is displayed
+            on the toolbar when the circle is clicked.
+          </p>
+          <img src={titleGif.node.publicURL} className="my-5 w-full" />
+          <p>
+            Obviously, this project can be improved. My future plans for this
+            project are to implement a search option that allows the user to
+            type in the movie's title. Other search filter algorithms should be
+            implemented as well such filter the movies by ratings. I'd also like
+            to improve the application in a way that would support a movie with
+            multiple types of genres because currently it can only switch
+            between any individual genre.
+          </p>
         </div>
         <div className="hidden sm:block sm:w-1/4 sm:pl-10">
           {sideInfo.map((i, iIndex) => (
