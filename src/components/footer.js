f5d833768957e466678af9a54ffff8956981b316
Heng Bodyka <heng.bodyka@sabay.com>
2020-11-10 19:36:53 +0700

    fixed publication column size

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,12 +10,16 @@ export default function footer() {
         Made with
         <FontAwesomeIcon
           icon={faHeart}
-          className="mx-2 hover:text-red"
+          className="mx-2 hover:text-red text-red sm:text-white"
           onClick={() => setIsVisible(!isVisible)}
         ></FontAwesomeIcon>
         and lots of JS.
       </div>
-      <div className={(isVisible ? "block" : "hidden") + " text-xs mx-5"}>
+      <div
+        className={
+          (isVisible ? "block" : "hidden") + " text-xs mx-5 text-right"
+        }
+      >
         Made with <span className="font-bold">MuyHourng</span>.
       </div>
     </div>
