7304f0a751e09faad3d7acbb2b93f03501abec7a
Heng Bodyka <heng.bodyka@sabay.com>
2020-11-10 18:18:52 +0700

    fixed footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Menu from "./menu";
+import Footer from "./footer";
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <div className="flex flex-col h-screen justify-between">
       <Menu></Menu>
       <div className="container mx-auto px-5 pb-10 sm:px-20">{children}</div>
+      <Footer className="block w-full"></Footer>
     </div>
   );
 }
